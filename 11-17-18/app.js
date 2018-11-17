8fc4ab9a18bb5c4baeb4d13ac230ec02ead02c5b
ardnahcivar <ravbhanage@gmail.com>
2018-11-17 11:41:01 +0530

    checked for nested enpoint css is loaded

---
+++ 11-17-18/app.js
@@ -0,0 +1,24 @@
+var express = require('express');
+var expressEdge = require('express-edge');
+var bodyParser = require('body-parser');
+
+const app = express()
+app.set('views', __dirname + '/views/layout');
+app.use(express.static('public'));
+app.use('/assets',express.static(__dirname + '/public/css'));
+app.use(expressEdge);
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+    extended: true
+}))
+
+app.get('/',(req,res) => {
+    res.render('index');
+})
+
+app.get('/posts/new',(req,res) => {
+    res.render('create');
+})
+
+app.listen(8081);
+
